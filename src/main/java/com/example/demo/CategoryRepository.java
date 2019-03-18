a11b3f25d971240ebb715dcd01e8fe96d270cc3d
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-03-18 03:58:24 -0400

    name change

--- src/main/java/com/example/demo/CategoryRepository.java
+++ src/main/java/com/example/demo/CategoryRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
-  Category findByTitle(String category_name);
+  Category findByTitle(String category_title);
 }
